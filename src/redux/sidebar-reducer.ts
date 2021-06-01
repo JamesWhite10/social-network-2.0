7ec18fdea82d51cadc93e34fe40c1abc9cc10fc3
JamesWhite10 <max.migalin10@gmail.com>
2021-06-01 14:27:25 +0300

    Refactoring.

--- src/redux/sidebar-reducer.ts
+++ src/redux/sidebar-reducer.ts
@@ -7,8 +7,8 @@ type InitialStateType = {
 const initialState: InitialStateType = {
     friends: [
         {id: 1, name: "Patrik", image: "https://img2.freepng.ru/20180318/wye/kisspng-derrick-rose-chicago-bulls-nba-draft-cleveland-cav-derrick-rose-cliparts-5aae255e6d7772.9388071515213622704484.jpg"},
-        {id: 1, name: "Ann", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlcU4bXsUHvbehYhD3UtlJWivLmu_7DOQ8Vw&usqp=CAU"},
-        {id: 1, name: "Oliver", image: "https://i.pinimg.com/originals/40/96/e1/4096e11a4e50eb8551db700a8aa96ba7.jpg"}
+        {id: 2, name: "Ann", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlcU4bXsUHvbehYhD3UtlJWivLmu_7DOQ8Vw&usqp=CAU"},
+        {id: 3, name: "Oliver", image: "https://i.pinimg.com/originals/40/96/e1/4096e11a4e50eb8551db700a8aa96ba7.jpg"}
     ]
 }
 
