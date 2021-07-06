e0c366ccd7254fd03d06e5c06e61315f7b0350e6
JamesWhite10 <max.migalin10@gmail.com>
2021-07-06 12:18:39 +0300

    Post form refactor.

--- src/common/FormControls/FormsControls.tsx
+++ src/common/FormControls/FormsControls.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import {WrappedFieldProps} from "redux-form";
-import classes from "./FormControl.module.css";
+import classes from "./FormsControls.module.css";
 
-export const Textarea: React.FC<WrappedFieldProps> = ({input, meta, ...props}) => {
-    const hasError = meta.touched && meta.error
+export const Textarea: React.FC<WrappedFieldProps> = ({input, meta: {touched, error}, ...props}) => {
+    const hasError = touched && error
     return (
         <div className={`${classes.formControl} ${hasError ? classes.error: ""}`}>
             <div>
                 <textarea {...input} {...props}/>
             </div>
-            {hasError && <span>{meta.error}</span>}
+            {hasError && <span className={classes.span}>{error}</span>}
         </div>
     )
 }
